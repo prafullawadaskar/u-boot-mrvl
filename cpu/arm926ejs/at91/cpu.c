e8c98754348d7518e2b8f7274539bbcce65e82bc
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2009-05-31 14:53:18 +0200

    at91: move cpu info print to cpu
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- cpu/arm926ejs/at91/cpu.c
+++ cpu/arm926ejs/at91/cpu.c
@@ -1,4 +1,4 @@
-#include <config.h>
+#include <common.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/at91_pmc.h>
 #include <asm/arch/clk.h>
@@ -12,3 +12,20 @@ int arch_cpu_init(void)
 	return at91_clock_init(0);
 #endif
 }
+
+#if defined(CONFIG_DISPLAY_CPUINFO)
+int print_cpuinfo(void)
+{
+	char buf[32];
+
+	printf("CPU: %s\n", AT91_CPU_NAME);
+	printf("Crystal frequency: %8s MHz\n",
+					strmhz(buf, get_main_clk_rate()));
+	printf("CPU clock        : %8s MHz\n",
+					strmhz(buf, get_cpu_clk_rate()));
+	printf("Master clock     : %8s MHz\n",
+					strmhz(buf, get_mck_clk_rate()));
+
+	return 0;
+}
+#endif
